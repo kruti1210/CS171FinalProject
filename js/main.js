0790a7075e4a059013229c2d3498a36f35caab6e
hwchase17 <hchase@college.harvard.edu>
2016-11-12 18:16:31 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 var areachart;
-var colorScale = d3.scale.category20();
-var allData = []
+var subchart;
+var allData = [];
 var general_keys = ["Out of pocket","Health Insurance","Other Third Party Payers and Programs","Public Health Activity","Investment"]
+var all_keys = {'Out of pocket':['Out of pocket'],'Health Insurance':
+    ['Private Health Insurance','Medicare','Medicaid (Title XIX)','CHIP (Title XIX and Title XXI)','' +
+    'Department of Defense','Department of Veterans Affairs'],'Other Third Party Payers and Programs':
+['Worksite Health Care','Other Private Revenues','Indian Health Services',"Workers' Compensation",
+'General Assistance','Maternal/Child Health','Vocational Rehabilitation','Other Federal Programs*',
+'SAMHSA','Other State and Local Programs**','School Health'],'Public Health Activity':['Federal','State and Local'],
+    'Investment':['Research','Structures & Equipment']
+};
 String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
@@ -16,8 +24,5 @@ d3.csv("data/nhe2014.csv", function(data) {
             d[d_1['Expenditure Amount (Millions)'].trim()] = +d_1[d.Year].replaceAll(',','')||0
         })
     });
-    console.log(allData)
-    colorScale.domain(general_keys)
-    console.log(d3.keys(allData[0]));
-    areachart = new StackedAreaChart("stacked-area-chart",allData);
+    areachart = new StackedAreaChart("stacked-area-chart",allData,general_keys);
 });
