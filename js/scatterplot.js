7a1ead051066f3f6219df5cadbf5376e6bd6854c
kruti1210 <kvora1210@gmail.com>
2016-11-26 17:03:31 -0500

    Update scatterplot.js

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -4,10 +4,12 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 850 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var xScale = d3.scale.linear().range([0, width]),
+var xValue,
+    xScale = d3.scale.linear().range([0, width]),
     xAxis1 = d3.svg.axis().scale(xScale).orient("bottom");
 
-var yScale = d3.scale.linear().range([height, 0]),
+var yValue,
+    yScale = d3.scale.linear().range([height, 0]),
     yAxis1 = d3.svg.axis().scale(yScale).orient("left");
 
 var popScale = d3.scale.linear().range([4,30]);
@@ -31,25 +33,46 @@ var tooltip = d3.select("#scatterplot").append("div")
     .attr("class", "scatter-tooltip")
     .style("opacity", 0);
 
-d3.csv("data/Accessibility.csv", function (error, data) {
-    data.forEach(function(d) {
-        d.Active_Physicians = +d.Active_Physicians;
-        d.Hospital_Bed_Density = +d.Hospital_Bed_Density;
-        d.Hospital_Beds = +d.Hospital_Beds;
-        d.Hospital_Bed_Density = d3.format(".1f")(d.Hospital_Bed_Density)
-        d.Population_100000 = +d.Population_100000;
-        d.Wait_Time = +d.Wait_Time;
-        d.Population = d3.format(",.0f")(d.Population_100000 * 100000)
-    });
+load();
 
-    xScale.domain([
-        d3.min(data, function(d) {return d.Active_Physicians;}) - 3,
-        d3.max(data, function(d) {return d.Active_Physicians;}) + 3
-    ]);
-    yScale.domain([
-        d3.min(data, function(d) {return d.Hospital_Bed_Density;}) - 3,
-        d3.max(data, function(d) {return d.Hospital_Bed_Density;}) + 3
-    ]);
+function load() {
+    d3.csv("data/Accessibility.csv", function (error, data) {
+        data.forEach(function(d) {
+            d.Active_Physicians = +d.Active_Physicians;
+            d.Hospital_Bed_Density = +d.Hospital_Bed_Density;
+            d.Hospital_Beds = +d.Hospital_Beds;
+            d.Hospital_Bed_Density = d3.format(".1f")(d.Hospital_Bed_Density)
+            d.Population_100000 = +d.Population_100000;
+            d.Wait_Time = +d.Wait_Time;
+            d.Population = d3.format(",.0f")(d.Population_100000 * 100000)
+        });
+
+        var selectBoxX = document.getElementById("scatter-x");
+        xValue = selectBoxX.options[selectBoxX.selectedIndex].value;
+
+        var selectBoxY = document.getElementById("scatter-y");
+        yValue = selectBoxY.options[selectBoxY.selectedIndex].value;
+
+        drawScatter(data);
+    });
+}
+
+function drawScatter(data) {
+    svg2.selectAll(".dot").remove();
+    svg2.selectAll(".axis").remove();
+    svg2.selectAll(".label").remove();
+    svg2.selectAll(".pop-label").remove();
+
+        xScale.domain([
+            d3.min(data, function(d) {return d[xValue];}) - 3,
+            d3.max(data, function(d) {return d[xValue];}) + 3
+        ]);
+
+        yScale.domain([
+            d3.min(data, function(d) {return d[yValue];}) - 3,
+            d3.max(data, function(d) {return d[yValue];}) + 3
+        ]);
+
     popScale.domain([
         d3.min(data, function(d) {return d.Population_100000}),
         d3.max(data, function(d) {return d.Population_100000})
@@ -64,7 +87,11 @@ d3.csv("data/Accessibility.csv", function (error, data) {
         .attr("x", width - 50)
         .attr("y", -6)
         .style("text-anchor", "end")
-        .text("Physicians per 100,000 population");
+        .text(function() {
+            if (xValue == "Active_Physicians") {return "Active physicians per 100,000 population"}
+            if (xValue == "Hospital_Bed_Density") {return "Hospital beds per 100,000 population"}
+            if (xValue == "Wait_Time") {return "Average emergency room wait time (min)"}
+        });
 
     // y-axis
     svg2.append("g")
@@ -76,7 +103,11 @@ d3.csv("data/Accessibility.csv", function (error, data) {
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Hospital beds per 100,000 population");
+        .text(function() {
+            if (yValue == "Active_Physicians") {return "Active physicians per 100,000 population"}
+            if (yValue == "Hospital_Bed_Density") {return "Hospital beds per 100,000 population"}
+            if (yValue == "Wait_Time") {return "Average emergency room wait time (min)"}
+        });
 
 
     svg2.selectAll(".dot")
@@ -84,8 +115,8 @@ d3.csv("data/Accessibility.csv", function (error, data) {
         .enter().append("circle")
         .attr("class", "dot")
         .attr("r", function(d) {return popScale(d.Population_100000);})
-        .attr("cx", function(d) { return xScale(d.Active_Physicians);})
-        .attr("cy", function(d) { return yScale(d.Hospital_Bed_Density);})
+        .attr("cx", function(d) { return xScale(d[xValue]);})
+        .attr("cy", function(d) { return yScale(d[yValue]);})
         .attr("opacity", 0.6)
         .style("stroke", "black")
         .style("fill", function(d) { return color2(cValue(d));})
@@ -99,9 +130,9 @@ d3.csv("data/Accessibility.csv", function (error, data) {
                 .style("top", (d3.event.pageY-28) + "px");
         })
         .on("mouseout", function(d) {
-           tooltip.transition()
-               .duration(200)
-               .style("opacity", 0);
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0);
         });
 
     var legend = svg2.selectAll(".legend")
@@ -126,21 +157,23 @@ d3.csv("data/Accessibility.csv", function (error, data) {
         .text(function(d) { return d;});
 
     svg2.append("text")
+        .attr("class", "pop-label")
         .attr("x", width-50)
         .attr("y", 100)
         .style("text-anchor", "end")
         .text("Circle radius corresponds");
 
     svg2.append("text")
+        .attr("class", "pop-label")
         .attr("x", width-50)
         .attr("y", 115)
         .style("text-anchor", "end")
         .text("to population size");
     console.log(data);
-});
+}
 
 function displayDetails (d) {
-    svg3.selectAll("text").remove()
+    svg3.selectAll("text").remove();
     svg3.append("text")
         .attr("class", "state")
         .attr("x", 10)
