444b5b2910f0fbaaa8d06a19de6dd591cf7c2b12
hwchase17 <hchase@college.harvard.edu>
2016-11-27 16:22:54 -0500

    Update stackedAreaChart.js

--- js/stackedAreaChart.js
+++ js/stackedAreaChart.js
@@ -35,6 +35,9 @@ StackedAreaChart.prototype.initVis = function(start_yr,end_yr){
 	var selectedData = vis.data.filter(function(d){
 		return ((d.Year>=start_yr)&(d.Year<=end_yr))
 	})
+	selectedData.forEach( function (d) {
+        console.log(d);
+    });
     console.log(selectedData);
     var transposedData = dataCategories.map(function(name) {
         return {
