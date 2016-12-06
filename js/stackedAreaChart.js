1defa4c629a037e3a04654a116c1276bc0236393
hwchase17 <hchase@college.harvard.edu>
2016-12-06 15:27:32 -0500

    Update stackedAreaChart.js

--- js/stackedAreaChart.js
+++ js/stackedAreaChart.js
@@ -1,23 +1,16 @@
-
-
-/*
- * StackedAreaChart - Object constructor function
- * @param _parentElement 	-- the HTML element in which to draw the visualization
- * @param _data						-- the  
- */
-
 StackedAreaChart = function(_parentElement, _data, _keys,_colorScale){
-	this.parentElement = _parentElement;
-  this.data = _data;
+    this.parentElement = _parentElement;
+    this.data = _data;
     this.keys = _keys;
-  this.displayData = []; // see data wrangling
-	this.colorScale = _colorScale;
+    this.displayData = []; // see data wrangling
+    this.colorScale = _colorScale;
 
-  // DEBUG RAW DATA
-	var start_year = document.getElementById('start').value;
-	var end_year = document.getElementById('end').value;
+    // DEBUG RAW DATA
+    var start_year = 1960;
+    var end_year = 2014;
 
-  this.initVis(start_year,end_year);
+
+    this.initVis(start_year,end_year);
 }
 
 
@@ -27,14 +20,14 @@ StackedAreaChart = function(_parentElement, _data, _keys,_colorScale){
  */
 
 StackedAreaChart.prototype.initVis = function(start_yr,end_yr){
-	console.log(this.parentElement);
-	document.getElementById(this.parentElement).innerHTML = "";
-	var vis = this;
+    console.log(this.parentElement);
+    document.getElementById(this.parentElement).innerHTML = "";
+    var vis = this;
     vis.colorScale.domain(vis.keys);
     var dataCategories = vis.colorScale.domain();
-	var selectedData = vis.data.filter(function(d){
-		return ((d.Year>=start_yr)&(d.Year<=end_yr))
-	});
+    var selectedData = vis.data.filter(function(d){
+        return ((d.Year>=start_yr)&(d.Year<=end_yr))
+    });
     console.log(selectedData);
     var transposedData = dataCategories.map(function(name) {
         return {
@@ -49,25 +42,25 @@ StackedAreaChart.prototype.initVis = function(start_yr,end_yr){
         .values(function(d) { return d.values; });
     vis.stackedData = stack(transposedData);
 
-	vis.margin = { top: 40, right: 0, bottom: 40, left: 80 };
+    vis.margin = { top: 40, right: 0, bottom: 40, left: 80 };
 
-	vis.width = 800 - vis.margin.left - vis.margin.right,
-  vis.height = 350 - vis.margin.top - vis.margin.bottom;
-	//Tooltip from http://bl.ocks.org/d3noob/c37cb8e630aaef7df30d
-	vis.tooltip = d3.select("body")
-	.append("div")
-	.style("position", "absolute")
-	.style("z-index", "10")
-	.style("visibility", "hidden")
-	.text("a simple tooltip");
+    vis.width = 800 - vis.margin.left - vis.margin.right,
+        vis.height = 350 - vis.margin.top - vis.margin.bottom;
+    //Tooltip from http://bl.ocks.org/d3noob/c37cb8e630aaef7df30d
+    vis.tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .text("a simple tooltip");
 
 
-  // SVG drawing area
-	vis.svg = d3.select("#" + vis.parentElement).append("svg")
-	    .attr("width", vis.width + vis.margin.left + vis.margin.right)
-	    .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
-	  .append("g")
-	    .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+    // SVG drawing area
+    vis.svg = d3.select("#" + vis.parentElement).append("svg")
+        .attr("width", vis.width + vis.margin.left + vis.margin.right)
+        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
 
     vis.svg.append("defs").append("clipPath")
@@ -75,49 +68,53 @@ StackedAreaChart.prototype.initVis = function(start_yr,end_yr){
         .append("rect")
         .attr("width", vis.width)
         .attr("height", vis.height);
-	// TO-DO: Overlay with path clipping
+    // TO-DO: Overlay with path clipping
 
 
-	// Scales and axes
-  vis.x = d3.scale.linear()
-  	.range([0, vis.width])
-  	.domain([start_yr,end_yr]);
+    // Scales and axes
+    var x_range = [1960,2014]
+    if(timeline.brush.empty()==false){
+        x_range = timeline.brush.extent();
+    }
+    vis.x = d3.scale.linear()
+        .range([0, vis.width])
+        .domain(x_range);
 
-	vis.y = d3.scale.linear()
-		.range([vis.height, 0]);
+    vis.y = d3.scale.linear()
+        .range([vis.height, 0]);
 
-	vis.xAxis = d3.svg.axis()
-		  .scale(vis.x)
-		  .orient("bottom").tickFormat(d3.format("d"));
+    vis.xAxis = d3.svg.axis()
+        .scale(vis.x)
+        .orient("bottom").tickFormat(d3.format("d"));
 
-	vis.yAxis = d3.svg.axis()
-	    .scale(vis.y)
-	    .orient("left");
+    vis.yAxis = d3.svg.axis()
+        .scale(vis.y)
+        .orient("left");
 
-	vis.svg.append("g")
-	    .attr("class", "x-axis axis")
-	    .attr("transform", "translate(0," + vis.height + ")");
+    vis.svg.append("g")
+        .attr("class", "x-axis axis")
+        .attr("transform", "translate(0," + vis.height + ")");
 
-	vis.svg.append("g")
-			.attr("class", "y-axis axis");
+    vis.svg.append("g")
+        .attr("class", "y-axis axis");
 
-	// TO-DO: Initialize stack layout
+    // TO-DO: Initialize stack layout
 
 
-  // TO-DO: Stacked area layout
-	vis.area = d3.svg.area()
+    // TO-DO: Stacked area layout
+    vis.area = d3.svg.area()
         .interpolate("cardinal")
         .x(function(d) { return vis.x(d.Year); })
         .y0(function(d) { return vis.y(d.y0); })
         .y1(function(d) { return vis.y(d.y0 + d.y); });
-	//	...
+    //	...
 
 
-	// TO-DO: Tooltip placeholder
+    // TO-DO: Tooltip placeholder
 
-	// TO-DO: (Filter, aggregate, modify data)
+    // TO-DO: (Filter, aggregate, modify data)
 
-  vis.wrangleData();
+    vis.wrangleData();
 }
 
 
@@ -127,12 +124,12 @@ StackedAreaChart.prototype.initVis = function(start_yr,end_yr){
  */
 
 StackedAreaChart.prototype.wrangleData = function(){
-	var vis = this;
+    var vis = this;
 
-	// In the first step no data wrangling/filtering needed
-	vis.displayData = vis.stackedData;
-	// Update the visualization
-  vis.updateVis();
+    // In the first step no data wrangling/filtering needed
+    vis.displayData = vis.stackedData;
+    // Update the visualization
+    vis.updateVis();
 }
 
 
@@ -142,54 +139,60 @@ StackedAreaChart.prototype.wrangleData = function(){
  * Function parameters only needed if different kinds of updates are needed
  */
 
-StackedAreaChart.prototype.updateVis = function(){
-	var vis = this;
+StackedAreaChart.prototype.updateVis = function() {
+    var vis = this;
 
-	// Update domain
-	// Get the maximum of the multi-dimensional array or in other words, get the highest peak of the uppermost layer
-	vis.y.domain([0, d3.max(vis.displayData, function(d) {
-			return d3.max(d.values, function(e) {
-				return e.y0 + e.y;
-			});
-		})
-	]);
-
-
-	// Draw the layers
-	var categories = vis.svg.selectAll(".area")
-      .data(vis.displayData);
-  
-  categories.enter().append("path")
-      .attr("class", "area")
-	  .on("mouseover", function(d){vis.tooltip.text(d.name);
-	  return vis.tooltip.style("visibility", "visible");})
-	  .on('mousemove',function(d){return vis.tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-	  .on("mouseout", function(d) {
-		  return vis.tooltip.style("visibility", "hidden")
-	  }).on("click", function(d){
-      if(general_keys.includes(d.name)){
-          document.getElementById('stacked-area-chart-sub').innerHTML = "";
-          subchart = new StackedAreaChart("stacked-area-chart-sub",allData,all_keys[d.name],d3.scale.category20c());
-      }
-  });
-
-  categories
-  		.style("fill", function(d) { 
-  			return vis.colorScale(d.name);
-  		})
-      .attr("d", function(d) {
-				return vis.area(d.values);
-      })
-
-  // TO-DO: Update tooltip text
+    // Update domain
+    // Get the maximum of the multi-dimensional array or in other words, get the highest peak of the uppermost layer
+    vis.y.domain([0, d3.max(vis.displayData, function (d) {
+            return d3.max(d.values, function (e) {
+                if(e.Year<=vis.x.domain()[1]+1) {
+                    return e.y0 + e.y;
+                }else{
+                    return 0;
+                }
+            });
+    })
+    ]);
 
 
+    // Draw the layers
+    var categories = vis.svg.selectAll(".area")
+        .data(vis.displayData);
+
+    categories.enter().append("path")
+        .attr("class", "area")
+        .on("mouseover", function (d) {
+            vis.tooltip.text(d.name);
+            return vis.tooltip.style("visibility", "visible");
+        })
+        .on('mousemove', function (d) {
+            return vis.tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
+        })
+        .on("mouseout", function (d) {
+            return vis.tooltip.style("visibility", "hidden")
+        }).on("click", function (d) {
+        if (general_keys.includes(d.name)) {
+            document.getElementById('stacked-area-chart-sub').innerHTML = "";
+            subchart = new StackedAreaChart("stacked-area-chart-sub", allData, all_keys[d.name], d3.scale.category20c());
+        }
+    });
+
+    categories
+        .style("fill", function (d) {
+            return vis.colorScale(d.name);
+        })
+        .attr("d", function (d) {
+            return vis.area(d.values);
+        })
+
+    // TO-DO: Update tooltip text
 
 
-	categories.exit().remove();
+    categories.exit().remove();
 
 
-	// Call axis functions with the new domain 
-	vis.svg.select(".x-axis").call(vis.xAxis);
-  vis.svg.select(".y-axis").call(vis.yAxis);
+    // Call axis functions with the new domain
+    vis.svg.select(".x-axis").call(vis.xAxis);
+    vis.svg.select(".y-axis").call(vis.yAxis);
 }
